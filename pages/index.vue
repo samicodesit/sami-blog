47a3758fe625e9f39d6ea48db00d6bb4c150ed3f
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-09 11:30:31 +0200

    feat: added netlify identity widget script

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,11 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        sami-blog
-      </h1>
-      <h2 class="subtitle">
-        My very personal blog
-      </h2>
+      <h1 class="title">sami-blog</h1>
+      <h2 class="subtitle">My very personal blog</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
@@ -28,6 +16,13 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  },
   components: {
     Logo
   }
