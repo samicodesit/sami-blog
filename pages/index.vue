be6f8cbdbbfb33264bc14cbeeb12cc81c1c45474
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-09 11:40:04 +0200

    feat: updated prettier to format on save

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,15 @@
       <h1 class="title">sami-blog</h1>
       <h2 class="subtitle">My very personal blog</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+          >GitHub</a
+        >
       </div>
     </div>
   </div>
