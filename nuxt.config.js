3e4686444aa18d9ec07a26f0f04f74a298383b02
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-09 12:20:24 +0200

    feat: blog list and post pages. updated nuxt generate config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,16 @@
 export default {
+  generate: {
+    routes: () => {
+      const fs = require('fs')
+      const path = require('path')
+      return fs.readdirSync('./assets/content/blog').map((file) => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./assets/content/blog/${file}`)
+        }
+      })
+    }
+  },
   mode: 'universal',
   /*
    ** Headers of the page
@@ -38,7 +50,10 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: ['@nuxtjs/markdownit'],
+  markdownit: {
+    injected: true
+  },
   /*
    ** Build configuration
    */
---
+++ pages/blog/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts
+    }
+  }
+}
+</script>
